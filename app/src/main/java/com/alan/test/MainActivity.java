f2383ecf238a216abefa25ac31cfea14095d83ba
Alan <tonyyxm@qq.com>
2020-03-30 14:16:48 +0800

    添加图片处理页面

--- app/src/main/java/com/alan/test/MainActivity.java
+++ app/src/main/java/com/alan/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.alan.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.alan.framework.view.loading.LoadingDialog;
 
@@ -13,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         LoadingDialog loadingDialog = new LoadingDialog(this);
         loadingDialog.show();
+
+        String path = getFilesDir().getPath();
+        Log.d("test_app", path);
     }
 }
